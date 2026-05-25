2a4901fe78a7601f18a5e7ad99b1de365f15c185
mvu46 <allisonvu9@gmail.com>
2026-05-25 18:23:02 -0400

    Created complete method.

--- src/main/java/com/example/payroll/OrderController.java
+++ src/main/java/com/example/payroll/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -82,4 +83,22 @@ class OrderController {
                 .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
     }
 
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+
+        Order order = orderRepository.findById(id)
+            .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+            .status(HttpStatus.METHOD_NOT_ALLOWED)
+            .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+            .body(Problem.create()
+                .withTitle("Method not allowed")
+                .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+    }
 }
